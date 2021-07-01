1c65a064fa57b89c731a3dd70289597f5d04924e
Harsh Solanki <hsolanki1884@gmail.com>
2021-07-01 18:54:59 +0530

    db setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
-import express from 'express';
-const app=express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
 
 
 
 
-const PORT=process.env.PORT||5000;
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`);
-})
+//DB connectivity
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+}).then(()=>console.log('connected db'))
+.catch(err=>console.error(err));
+
+
+
+//server startup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
