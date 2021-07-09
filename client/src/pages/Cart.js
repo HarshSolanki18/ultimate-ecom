8c8ec27bcbbc59ef3a7e80d911ea94a6c56a612c
Harsh Solanki <hsolanki1884@gmail.com>
2021-07-09 16:13:27 +0530

    cart ui finally finished

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import { Card, Divider } from "@material-ui/core";
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  Divider,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -25,38 +34,129 @@ const cartItems = [
 ];
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    "& > *": {
-      margin: theme.spacing(2),
-      padding: theme.spacing(1),
-    },
-    root: {
-        margin: theme.spacing(2),
-        padding: theme.spacing(1),
-    },
+  root: {
+    margin: theme.spacing(1),
+    padding: theme.spacing(2),
   },
+  top: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginBottom: theme.spacing(1),
+  },
+
+  image: {
+    height: "auto",
+    width: "100%",
+    maxWidth: "400px",
+  },
+  card: {
+    display: "flex",
+    margin: theme.spacing(1),
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+  qty: {
+    display:'flex',
+    },
+  qtyItem:{
+    marginRight:theme.spacing(2),
+    marginTop:theme.spacing(2),
+    marginBottom:theme.spacing(2)
+    
+  }  
 }));
 const Cart = () => {
   const classes = useStyles();
+  const [age, setAge] = useState(0);
   return (
-      <div className={classes.paper}>
-    <Card className={classes.root} variant='outlined'>
+    <div className={classes.root}>
+      <Paper variant="outlined" className={classes.paper}>
+        <div className={classes.top}>
+          <Typography variant="h6" component="h2">
+            My Cart({cartItems.length})
+          </Typography>
+          <Button size="small" color="primary" variant="outlined">
+            Remove All
+          </Button>
+        </div>
+        <Divider />
+        <div className={classes.top}>
+          <Typography variant="h6" component="h2">
+            Product Name whic is big brand
+          </Typography>
+          <Typography variant="h6" component="h6">
+            Delivered by Fri,16
+          </Typography>
+        </div>
+        <Typography variant="body1" component="p" color="textSecondary">
+          Company Name
+        </Typography>
 
-      <CardActionArea>
-        
-        
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Shopping cart
+        <Typography variant="body1" component="h2" bold>
+          Price : 250${" "}
+          <span style={{ color: "grey", textDecoration: "line-through" }}>
+            200$
+          </span>{" "}
+          <span style={{ color: "green" }}>29% off</span>{" "}
+        </Typography>
+        <div className={classes.qty}>
+          <Typography component="p" variant="h6" className={classes.qtyItem}>Qty:</Typography>
+          <FormControl className={classes.qtyItem}>
+            
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <Button size="small" variant="outlined" color="primary" className={classes.qtyItem}>Remove</Button>
+        </div>
+        <Divider />
+        {/* checking the responsivesness*/}
+        <div className={classes.top}>
+          <Typography variant="h6" component="h2">
+            Product Name whic is big brand
           </Typography>
-          
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          <Typography variant="h6" component="h6">
+            Delivered by Fri,16
           </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+        </div>
+        <Typography variant="body1" component="p" color="textSecondary">
+          Company Name
+        </Typography>
+
+        <Typography variant="body1" component="h2" bold>
+          Price : 250${" "}
+          <span style={{ color: "grey", textDecoration: "line-through" }}>
+            200$
+          </span>{" "}
+          <span style={{ color: "green" }}>29% off</span>{" "}
+        </Typography>
+        <div className={classes.qty}>
+          <Typography component="p" variant="h6" className={classes.qtyItem}>Qty:</Typography>
+          <FormControl className={classes.qtyItem}>
+            
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+          <Button size="small" variant="outlined" color="primary" className={classes.qtyItem}>Remove</Button>
+        </div>
+        <Divider />
+      </Paper>
     </div>
   );
 };
